7ccf237119195cbdeaaf82474b1a6dd35cc93f13
hrishi82 <hrishib4882@gmail.com>
2023-12-18 02:17:22 +0530

    commit

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -1,7 +1,6 @@
 import { Link as MUILink } from "@mui/material";
 import { NavLink } from "react-router-dom";
 import { Typography } from "@mui/material";
-import logo from "../../assets/logo.png";
 import { Box } from "@mui/material";
 
 interface NavBarPropsT {
